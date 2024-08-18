504e6484c81de979e9569411dabbd89079d6fac8
omwomo <eugeneoduor79@gmail.com>
2024-08-18 11:20:41 +0300

    Configure eslint linter.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"

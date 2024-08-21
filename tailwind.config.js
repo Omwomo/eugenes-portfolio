7898ecde82b9ae9958083d7778fc722195b70897
omwomo <eugeneoduor79@gmail.com>
2024-08-21 10:10:04 +0300

    Add coor themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: {
+          100: '#292f36',
+          200: '#43454d',
+          300: '#1a1e23',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};

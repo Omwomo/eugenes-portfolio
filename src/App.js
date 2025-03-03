dbd028fd3f9b916917314ee054c7c7bc963df0fc
omwomo <eugeneoduor79@gmail.com>
2025-03-03 11:31:41 +0300

    Import neew components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import AboutMe from './components/AboutMe';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <h1>Hello</h1>
       <Header />
       <Hero />
+      <AboutMe />
+      <Skills />
+      <Contact />
     </div>
   );
 }
